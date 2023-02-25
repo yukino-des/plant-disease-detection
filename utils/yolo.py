6f0366ebf8b69c23395cbe4a9854abcc93e614c3
yukino <3181137349@qq.com>
2023-02-25 19:35:53 +0800

    お疲れ様でした

--- utils/yolo.py
+++ utils/yolo.py
@@ -118,7 +118,7 @@ def fit_one_epoch(model_train, model, yolo_loss, loss_history, eval_callback, op
         print("Epoch: " + str(epoch + 1) + "/" + str(_epoch_))
         print("Total Loss: %.3f; Val Loss: %.3f" % (loss / epoch_step, val_loss / epoch_step_val))
         if (epoch + 1) % save_period == 0 or epoch + 1 == _epoch_:
-            torch.save(model.state_dict(), os.path.join(save_dir, "ep%03d-loss%.3f-val_loss%.3f.pth" % (
+            torch.save(model.state_dict(), os.path.join(save_dir, "epoch%03dloss%.3fval_loss%.3f.pth" % (
                 epoch + 1, loss / epoch_step, val_loss / epoch_step_val)))
         if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
             print("best.pth saved.")
